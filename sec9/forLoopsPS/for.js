fa1e4853573c159987b0b675247025494fbefbb0
irishmac473 <irishmac473@gmail.com>
2016-04-18 21:58:05 -0500

    Add For Loops PS to sec9

---
+++ sec9/forLoopsPS/for.js
@@ -0,0 +1,29 @@
+console.log("Print all numbers between -10 and 19");
+
+for(var i = -10; i < 20; i++) {
+  console.log(i);
+}
+
+console.log("Print all even numbers between 10 and 40");
+
+for(i = 10; i < 41; i++) {
+  if(i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+console.log("Print all odd numbers between 300 and 333");
+
+for(i = 300; i < 334; i++) {
+  if(i % 2 === 1) {
+  console.log(i);
+  }
+}
+
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50");
+
+for(i = 5; i < 51; i++) {
+  if(i % 3 === 0 && i % 5 === 0)  {
+    console.log(i);
+  }
+}
